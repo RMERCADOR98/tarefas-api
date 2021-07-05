7dcf79e9225578223aeec2b4de53eb1b4aa9cce0
RMERCADOR98 <rodrigosmercador@gmail.com>
2021-07-06 00:22:31 +0100

    test 3

--- src/Controllers/controller.js
+++ src/Controllers/controller.js
@@ -4,83 +4,97 @@ import Tarefas from "../Models/bloco.model";
 db.sync();
 
 const todasTarefas = async (req, res) => {
-  const data = await Tarefas.findAll().catch((err) => {
-    return res.sendStatus(500);
-  });
-
-  res.json(data);
+  try {
+    const data = await Tarefas.findAll();
+    res.json(data);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 const fazerTarefas = async (req, res) => {
-  const data = await Tarefas.findAll({
-    where: {
-      estado: "fazer",
-    },
-  }).catch((err) => {
-    return res.sendStatus(500);
-  });
-  res.json(data);
+  try {
+    const data = await Tarefas.findAll({
+      where: {
+        estado: "fazer",
+      },
+    });
+    res.json(data);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
+
 const fazendoTarefas = async (req, res) => {
-  const data = await Tarefas.findAll({
-    where: {
-      estado: "fazendo",
-    },
-  }).catch((err) => {
-    return res.sendStatus(500);
-  });
-  res.json(data);
+  try {
+    const data = await Tarefas.findAll({
+      where: {
+        estado: "fazendo",
+      },
+    });
+    res.json(data);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
+
 const feitoTarefas = async (req, res) => {
-  const data = await Tarefas.findAll({
-    where: {
-      estado: "feito",
-    },
-  }).catch((err) => {
-    return res.sendStatus(500);
-  });
-  res.json(data);
+  try {
+    const data = await Tarefas.findAll({
+      where: {
+        estado: "feito",
+      },
+    });
+    res.json(data);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 const criarTarefa = async (req, res) => {
   //   const data = await Tarefas.create(req.body);
 
-  const { tarefa, estado } = req.body;
-
-  const data = await Tarefas.create({
-    tarefa: tarefa,
-    estado: estado,
-  }).catch((err) => {
-    return res.sendStatus(500);
-  });
-
-  res.json(data);
+  try {
+    const { tarefa, estado } = req.body;
+    const data = await Tarefas.create({
+      tarefa: tarefa,
+      estado: estado,
+    });
+    res.json(data);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 const apagarTarefa = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  await Tarefas.destroy({
-    where: {
-      id: id,
-    },
-  }).catch((err) => {
-    return res.sendStatus(500);
-  });
-  res.sendStatus(200);
+    await Tarefas.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.sendStatus(200);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 const editarTarefa = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const data = await Tarefas.update(req.body, {
-    where: {
-      id: id,
-    },
-  }).catch((err) => {
-    return res.sendStatus(500);
-  });
-  res.json(data);
+    const data = await Tarefas.update(req.body, {
+      where: {
+        id: id,
+      },
+    });
+
+    res.json(data);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 // const hello = (req, res) => {
